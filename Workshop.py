feb993c73a0df4519c30afe508a7de53ce9a006f
Juan Camilo Vargas - UNSC 225 <juan.vargas_velez@uao.edu.co>
2023-08-27 22:43:07 -0500

    Update Queries
    Solve the problem with mathplotlib

--- Workshop.py
+++ Workshop.py
@@ -1,5 +1,6 @@
 import psycopg2
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def connect_postgres():
     connection = psycopg2.connect(
@@ -47,7 +48,7 @@ def insert_to_table():
 def Query1():
     connection = connect_postgres()
     cursor = connection.cursor()
-    cursor.execute("SELECT technology, COUNT(*) AS resultado_tech FROM candidates WHERE code_challenge_score >= 6 AND technical_interview_score >=6 GROUP BY technology")
+    cursor.execute("SELECT technology, COUNT(*) AS resultado_tech FROM candidates WHERE code_challenge_score >= 7 AND technical_interview_score >=7 GROUP BY technology")
     results = cursor.fetchall()
     tecnologies = []
     for row in results:
@@ -60,10 +61,11 @@ def Query1():
     plt.title("Employee Count by Department")
     plt.show()
 
-def Query2(): #################################### REVISAR - MAL EN ESCALAS
+
+def Query2():
     connection = connect_postgres()
     cursor = connection.cursor()
-    cursor.execute("SELECT EXTRACT(year FROM application_date) AS year, COUNT(application_date) AS conteo FROM candidates WHERE code_challenge_score >= 6 AND technical_interview_score >=6 GROUP BY year")
+    cursor.execute("SELECT EXTRACT(year FROM application_date) AS year, COUNT(application_date) AS conteo FROM candidates WHERE code_challenge_score >= 7 AND technical_interview_score >=7 GROUP BY year")
     results = cursor.fetchall()
     year = []
     for row in results:
@@ -72,15 +74,15 @@ def Query2(): #################################### REVISAR - MAL EN ESCALAS
 
     years = [tupla[0] for tupla in year]
     employees = [tupla[1] for tupla in year]
-
-    plt.barh(years,employees)
+    colors = ["#09607d", "#2b3266", "#1d7d72","#1d7d72","#3f1d7d"]
+    plt.barh(years,employees, color = colors)
     plt.title("Employee Count by year")
     plt.show()
 
 def Query3():
     connection = connect_postgres()
     cursor = connection.cursor()
-    cursor.execute("SELECT seniority, COUNT(*) AS resultado_seniority FROM candidates GROUP BY seniority")
+    cursor.execute("SELECT seniority, COUNT(seniority) AS resultado_seniority FROM candidates WHERE code_challenge_score >= 7 AND technical_interview_score >=7 GROUP BY seniority")
     results = cursor.fetchall()
     seniorits = []
     for row in results:
@@ -94,28 +96,37 @@ def Query3():
     plt.show()
 
 def Query4():
+    countries_list = ['United States of America', 'Brazil', 'Colombia', 'Ecuador']
+    colors = ["#09607d", "#2b3266", "#1d7d72","#1d7d72","#3f1d7d"]
     connection = connect_postgres()
     cursor = connection.cursor()
-    cursor.execute("SELECT country, COUNT(country) as co, CAST(EXTRACT(year FROM application_date)AS INT) AS year FROM candidates WHERE country IN ('United States of America', 'Brazil', 'Colombia', 'Ecuador') AND code_challenge_score >= 6 AND technical_interview_score >=6 GROUP BY country, year ORDER BY year")
+    cursor.execute("""
+        SELECT country, COUNT(country) as co, CAST(EXTRACT(year FROM application_date)AS INT) AS year
+        FROM candidates
+        WHERE country IN ('United States of America', 'Brazil', 'Colombia', 'Ecuador')
+        AND code_challenge_score >= 7
+        AND technical_interview_score >=7
+        GROUP BY country, year
+        ORDER BY year
+    """)
     results = cursor.fetchall()
-    countries = []
+    countries =[]
     for row in results:
-        countries.append([row[0], row[1], row[2]])
-    print(countries)
+        countries.append(row)
+    df = pd.DataFrame(countries)
+    df.columns = ['country','employes','year']
+    print(df)
     plt.figure(figsize=(10, 6))
-    for country, year, co in countries:
-        plt.plot(co, year, label=country)
-    plt.title("Employee Count by Country and Ordered by Year")
-    plt.xlabel("Year")
-    plt.ylabel("Number of hires")
-
-# Get unique countries
-    unique_countries = set()
-    for country, _, _ in countries:
-        unique_countries.add(country)
-
-# Set the legend labels to the unique countries
-    plt.legend(list(unique_countries))
+    #
+    for country in countries_list:
+        by_country = df[df['country'] == country]
+        plt.plot(by_country['year'], by_country['employes'], marker='o', label=country)
+        
+    plt.xlabel('Year')
+    plt.ylabel('Hires_count')
+    plt.title('Hires by country over years')
+    plt.legend(df['country'].unique())
+    plt.xticks(df['year'].unique())
     plt.show()
 
 if __name__ == "__main__":
